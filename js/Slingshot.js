8fadfd4ea620f0a5969aa1bb28d2cd75ce8bedfe
Prabanjan-A <77002754+Prabanjan-A@users.noreply.github.com>
2021-07-25 15:58:04 +0530

    Add files via upload

---
+++ js/Slingshot.js
@@ -0,0 +1,23 @@
+class Slingshot{
+  constructor(bodyA,bodyB)
+     var option = {
+         bodyA, bodyA,
+         pointB, pointB,
+         stiffness: 0.4,
+         length: 10   
+     }
+     this.pointB - pointB
+     this.sling - Constraint.create(option)
+     world.add(world, this.sling)
+}
+
+    fly(){
+        this.sling.bodyA - A
+    }
+
+    display(){
+        var pointA-this.sling.bodyA.position;
+        var pointB-this.pointB
+        strokeWeight(4);
+        line(pointA,x,pointA,y,pointB,x,pointB,y);
+    }
